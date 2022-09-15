0009b22d60f6ded996c6aecd53be1d344be3647e
Siiickzer <suphanath_w@cmu.ac.th>
2022-09-15 14:50:42 +0700

    finished

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 import Reply from "./Reply";
+import LikeCount from "./LikeCount";
 
 export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.profile_img}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.owner}
+          </span>
+          <br />
+          <span style={{ color: "#B0B3B8" }}>{props.content}</span>
+          <LikeCount liked={props.liked} />
+        </div>
+      </div>
 
       {/*render Reply here... */}
+      {props.replies.map((x, i) => {
+        return (
+          <Reply
+            key={i}
+            owner={x.username}
+            content={x.replyText}
+            profile_img={x.userImagePath}
+            liked={x.likeNum}
+          />
+        );
+      })}
     </div>
   );
 }
